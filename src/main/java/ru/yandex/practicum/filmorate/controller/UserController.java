64f02de60d1b3025b7fc1e28d0d2eebc58bbb917
Aleksander <las.l@mail.ru>
2025-09-10 20:55:09 +0300

    промежуточное сохранение 10ФЗ

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,67 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+    private final Map<Integer, User> users = new HashMap<>();
+    private int nextId = 1;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        log.info("Получен запрос на получение всех пользователей. Количество: {}", users.size());
+        return new ArrayList<>(users.values());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createUser(@Valid @RequestBody User user) {
+        validateUser(user);
+        user.setId(nextId++);
+
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+            log.info("Для пользователя {} установлено имя из логина: {}", user.getId(), user.getLogin());
+        }
+
+        users.put(user.getId(), user);
+        log.info("Добавлен новый пользователь: {}", user);
+        return user;
+    }
+
+    @PutMapping
+    public User updateUser(@Valid @RequestBody User user) {
+        if (user.getId() == null || !users.containsKey(user.getId())) {
+            log.warn("Попытка обновления несуществующего пользователя с id: {}", user.getId());
+            throw new ValidationException("Пользователь с id " + user.getId() + " не найден");
+        }
+        validateUser(user);
+
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+
+        users.put(user.getId(), user);
+        log.info("Обновлен пользователь: {}", user);
+        return user;
+    }
+
+    private void validateUser(User user) {
+        if (user.getLogin().contains(" ")) {
+            log.warn("Логин пользователя содержит пробелы: {}", user.getLogin());
+            throw new ValidationException("Логин не может содержать пробелы");
+        }
+    }
+}
