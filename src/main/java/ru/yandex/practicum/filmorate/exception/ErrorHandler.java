87b5d128d64cd86f6bae8ce1cc6dfc5c70a4d42b
Aleksander <las.l@mail.ru>
2025-09-15 20:26:11 +0300

    2 промежуточное сохранение 10ФЗ

--- src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
@@ -2,9 +2,13 @@ package ru.yandex.practicum.filmorate.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 @Slf4j
@@ -12,15 +16,31 @@ public class ErrorHandler {
 
     @ExceptionHandler(ValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleValidationException(ValidationException e) {
+    public Map<String, String> handleValidationException(ValidationException e) {
         log.warn("Ошибка валидации: {}", e.getMessage());
-        return e.getMessage();
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Validation Error");
+        error.put("message", e.getMessage());
+        return error;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.warn("Ошибка валидации аргументов: {}", e.getMessage());
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Argument Validation Error");
+        error.put("message", e.getFieldError().getDefaultMessage());
+        return error;
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String handleException(Exception e) {
+    public Map<String, String> handleException(Exception e) {
         log.error("Внутренняя ошибка сервера: {}", e.getMessage(), e);
-        return "Произошла внутренняя ошибка сервера";
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Internal Server Error");
+        error.put("message", "Произошла внутренняя ошибка сервера");
+        return error;
     }
 }
