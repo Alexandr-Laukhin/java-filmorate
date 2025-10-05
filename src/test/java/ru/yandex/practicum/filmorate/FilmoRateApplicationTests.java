3dfab0d7051d415c7c52ba537462307b12690eb4
Aleksander <las.l@mail.ru>
2025-10-06 02:07:24 +0300

    промежуточное 12ФЗ

--- src/test/java/ru/yandex/practicum/filmorate/FilmoRateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmoRateApplicationTests.java
@@ -2,12 +2,10 @@ package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-@ActiveProfiles("database")
 class FilmoRateApplicationTests {
 
     @Test
