47d6a508cbf002565900b070fc0ab7988308ae76
Aleksander <las.l@mail.ru>
2025-10-06 01:02:54 +0300

    промежуточное 12ФЗ

--- src/test/java/ru/yandex/practicum/filmorate/FilmoRateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmoRateApplicationTests.java
@@ -20,7 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 @Import({UserDbStorage.class})
-class FilmorateApplicationTests {
+class FilmoRateApplicationTests {
     private final UserDbStorage userStorage;
     private User testUser;
     private User createdUser;
